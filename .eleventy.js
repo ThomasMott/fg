863c17a6f90566549eb8f58d196352e1d33e93c4
DESKTOP-0GVB77S\thoma <thomasnmott1997@gmail.com>
2022-09-27 22:28:53 +0100

    Restyling

--- .eleventy.js
+++ .eleventy.js
@@ -34,7 +34,7 @@ module.exports = function (eleventyConfig) {
 		if (number > 0) {
 			relatedFilter.push(collection[number - 1]);
 		}
-		if (collection.length == (number - 1)) {
+		if ((collection.length - 1) !== number) {
 			relatedFilter.push(collection[number + 1]);
 		}
 		return relatedFilter;
